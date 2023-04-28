c7e794c6c2e11c535ae16e6f0a43376279d37b5f
Murilo Souza Almeida <murilo.souza.senai@gmail.com>
2023-04-28 15:14:32 -0300

    Criação da classe Murilo

---
+++ Projeto-Teste/Murilo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Teste
+{
+    public abstract class Murilo
+    {
+        public string Nome {get; set;}
+
+        public abstract void MostarNome();
+    }
+}
